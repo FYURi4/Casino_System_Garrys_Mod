2ea1632f326a38f4b223a3cb5855323e8d34da1e
FURUSHKA <163753122+FYURi4@users.noreply.github.com>
2025-07-13 10:41:45 -0700

    Create shared.lua

---
+++ lua/entities/vending_casino_v1/shared.lua
@@ -0,0 +1,24 @@
+ENT.Base = "base_gmodentity"
+
+ENT.Type = "anim"
+
+ENT.Category = "3DEMC_ENT"
+
+ENT.Spawnable = true
+
+ENT.AdminSpawnable = true
+
+ENT.ClassName = "Slot_Machines"
+
+ENT.PrintName = "Slot_Machines"
+
+ENT.Author = "FURA"
+
+ENT.Contact = "Discord: fyurl4, Furushka: FurichF"
+
+ENT.Purpose = ""
+
+ENT.Instructions = ""
+
+ENT.CameraOffset = Vector(15, 0, 0)
+ENT.CameraFOV = 75
