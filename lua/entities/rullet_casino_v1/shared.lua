55eef74b95cd0f90afa5bc5625b580a20d0683d9
FURUSHKA <163753122+FYURi4@users.noreply.github.com>
2025-07-15 03:19:52 -0700

    Update shared.lua

--- lua/entities/rullet_casino_v1/shared.lua
+++ lua/entities/rullet_casino_v1/shared.lua
@@ -20,9 +20,9 @@ ENT.Purpose = ""
 
 ENT.Instructions = ""
 
-CHIP_HEIGHT = 0.3 -- Высота одной фишки
+CHIP_HEIGHT = 0.15-- Высота одной фишки
 MAX_CHIP_STACK = 10 -- Максимальное количество фишек в стопке
-CHIP_OFFSET_RANDOM = 0.1 -- Случайное смещение фишек в стопке
+CHIP_OFFSET = 0.05
 
 CHIP_VALUES = {
     [0] = 1,    -- Текстура 0 = фишка 1
@@ -38,60 +38,77 @@ CHIP_MODEL = "models/darkrpcasinoby3demc/fishka_casino.mdl"
 
 POSITION = {
     ["OUTSIDE BETS"] = {
-        ["low (1to18)"] = {pos = Vector(12.8, 5, 17.8), ang = Angle(0, 90, 0)},
-        ["even"] = {pos = Vector(30, -20, 30), ang = Angle(0, 0, 0)},
-        ["on red"] = {pos = Vector(30, 0, 30), ang = Angle(0, 0, 0)},
-        ["on black"] = {pos = Vector(30, 20, 30), ang = Angle(0, 0, 0)},
-        ["odd"] = {pos = Vector(30, 40, 30), ang = Angle(0, 0, 0)},
-        ["1-12 (1st dozen)"] = {pos = Vector(-20, -50, 30), ang = Angle(0, 0, 0)},
-        ["13-24 (2st dozen)"] = {pos = Vector(-20, -25, 30), ang = Angle(0, 0, 0)},
-        ["25-36 (3rd dozen)"] = {pos = Vector(-20, 0, 30), ang = Angle(0, 0, 0)},
-        ["1 line (2to1)"] = {pos = Vector(0, 50, 30), ang = Angle(0, 0, 0)},
-        ["2 line (2to1)"] = {pos = Vector(0, 25, 30), ang = Angle(0, 0, 0)},
-        ["3 line (2to1)"] = {pos = Vector(0, 0, 30), ang = Angle(0, 0, 0)}
+        ["low (1to18)"] = {pos = Vector(17.7, 0, 17.8), ang = Angle(0, 90, 0)},
+        ["even"] = {pos = Vector(17.7, 9.5, 17.8), ang = Angle(0, 90, 0)},
+        ["on red"] = {pos = Vector(17.7, 19.4, 17.8), ang = Angle(0, 90, 0)},
+        ["on black"] = {pos = Vector(17.7, 29, 17.8), ang = Angle(0, 90, 0)},
+        ["odd"] = {pos = Vector(17.7, 38.7, 17.8), ang = Angle(0, 90, 0)},
+        ["high (10to36)"] = {pos = Vector(17.7, 48.45, 17.8), ang = Angle(0, 90, 0)},
+        --//                                                                     //--
+        ["1-12 (1st dozen)"] = {pos = Vector(12.8, 4.8, 17.8), ang = Angle(0, 90, 0)},
+        ["13-24 (2st dozen)"] = {pos = Vector(12.8, 24.2, 17.8), ang = Angle(0, 90, 0)},
+        ["25-36 (3rd dozen)"] = {pos = Vector(12.8, 43.5, 17.8), ang = Angle(0, 90, 0)},
+        --//                                                                     //--
+        ["1 line (2to1)"] = {pos = Vector(-5.4, 55.6, 17.8), ang = Angle(0, 90, 0)},
+        ["2 line (2to1)"] = {pos = Vector(1, 55.6, 17.8), ang = Angle(0, 90, 0)},
+        ["3 line (2to1)"] = {pos = Vector(7.3, 55.6, 17.8), ang = Angle(0, 90, 0)}
     },
     ["GREEN"] = {
-        ["0"] = {pos = Vector(-40, 50, 30), ang = Angle(0, 0, 0)} 
+        ["0"] = {pos = Vector(1, -7.15, 17.8), ang = Angle(0, 90, 0)} 
     },
     ["BLACK"] = {
-        ["1"] = {pos = Vector(-10, 40, 30), ang = Angle(0, 0, 0)},
-        ["3"] = {pos = Vector(-10, 35, 30), ang = Angle(0, 0, 0)},
-        ["5"] = {pos = Vector(-10, 30, 30), ang = Angle(0, 0, 0)},
-        ["7"] = {pos = Vector(-10, 25, 30), ang = Angle(0, 0, 0)},
-        ["9"] = {pos = Vector(-10, 20, 30), ang = Angle(0, 0, 0)},
-        ["11"] = {pos = Vector(-10, 15, 30), ang = Angle(0, 0, 0)},
-        ["13"] = {pos = Vector(-10, 10, 30), ang = Angle(0, 0, 0)},
-        ["15"] = {pos = Vector(-10, 5, 30), ang = Angle(0, 0, 0)},
-        ["17"] = {pos = Vector(-10, 0, 30), ang = Angle(0, 0, 0)},
-        ["19"] = {pos = Vector(-10, -5, 30), ang = Angle(0, 0, 0)},
-        ["21"] = {pos = Vector(-10, -10, 30), ang = Angle(0, 0, 0)},
-        ["23"] = {pos = Vector(-10, -15, 30), ang = Angle(0, 0, 0)},
-        ["25"] = {pos = Vector(-10, -20, 30), ang = Angle(0, 0, 0)},
-        ["27"] = {pos = Vector(-10, -25, 30), ang = Angle(0, 0, 0)},
-        ["29"] = {pos = Vector(-10, -30, 30), ang = Angle(0, 0, 0)},
-        ["31"] = {pos = Vector(-10, -35, 30), ang = Angle(0, 0, 0)},
-        ["33"] = {pos = Vector(-10, -40, 30), ang = Angle(0, 0, 0)},
-        ["35"] = {pos = Vector(-10, -45, 30), ang = Angle(0, 0, 0)}
+        ["1"] = {pos = Vector(7.3, -2.3, 17.8), ang = Angle(0, 90, 0)},
+        ["3"] = {pos = Vector(-5.4, -2.3, 17.8), ang = Angle(0, 90, 0)},
+        ["5"] = {pos = Vector(1, 2.47, 17.8), ang = Angle(0, 90, 0)},
+        ["7"] = {pos = Vector(7.3, 7.25, 17.8), ang = Angle(0, 90, 0)},
+        ["9"] = {pos = Vector(-5.4, 7.25, 17.8), ang = Angle(0, 90, 0)},
+        ["11"] = {pos = Vector(1, 12.1, 17.8), ang = Angle(0, 90, 0)},
+        ["13"] = {pos = Vector(7.3, 17, 17.8), ang = Angle(0, 90, 0)},
+        ["15"] = {pos = Vector(-5.4, 17, 17.8), ang = Angle(0, 90, 0)},
+        ["17"] = {pos = Vector(1, 21.78, 17.8), ang = Angle(0, 90, 0)},
+        ["19"] = {pos = Vector(7.3, 26.6, 17.8), ang = Angle(0, 90, 0)},
+        ["21"] = {pos = Vector(-5.4, 26.6, 17.8), ang = Angle(0, 90, 0)},
+        ["23"] = {pos = Vector(1, 31.5, 17.8), ang = Angle(0, 90, 0)},
+        ["25"] = {pos = Vector(7.3, 36.3, 17.8), ang = Angle(0, 90, 0)},
+        ["27"] = {pos = Vector(-5.4, 36.3, 17.8), ang = Angle(0, 90, 0)},
+        ["29"] = {pos = Vector(1, 41.2, 17.8), ang = Angle(0, 90, 0)},
+        ["31"] = {pos = Vector(7.3, 45.95, 17.8), ang = Angle(0, 90, 0)},
+        ["33"] = {pos = Vector(-5.4, 45.95, 17.8), ang = Angle(0, 90, 0)},
+        ["35"] = {pos = Vector(1, 50.85, 17.8), ang = Angle(0, 90, 0)}
     },
     ["RED"] = {
-        ["2"] = {pos = Vector(0, 40, 30), ang = Angle(0, 0, 0)},
-        ["4"] = {pos = Vector(0, 35, 30), ang = Angle(0, 0, 0)},
-        ["6"] = {pos = Vector(0, 30, 30), ang = Angle(0, 0, 0)},
-        ["8"] = {pos = Vector(0, 25, 30), ang = Angle(0, 0, 0)},
-        ["10"] = {pos = Vector(0, 20, 30), ang = Angle(0, 0, 0)},
-        ["12"] = {pos = Vector(0, 15, 30), ang = Angle(0, 0, 0)},
-        ["14"] = {pos = Vector(0, 10, 30), ang = Angle(0, 0, 0)},
-        ["16"] = {pos = Vector(0, 5, 30), ang = Angle(0, 0, 0)},
-        ["18"] = {pos = Vector(0, 0, 30), ang = Angle(0, 0, 0)},
-        ["20"] = {pos = Vector(0, -5, 30), ang = Angle(0, 0, 0)},
-        ["22"] = {pos = Vector(0, -10, 30), ang = Angle(0, 0, 0)},
-        ["24"] = {pos = Vector(0, -15, 30), ang = Angle(0, 0, 0)},
-        ["26"] = {pos = Vector(0, -20, 30), ang = Angle(0, 0, 0)},
-        ["28"] = {pos = Vector(0, -25, 30), ang = Angle(0, 0, 0)},
-        ["30"] = {pos = Vector(0, -30, 30), ang = Angle(0, 0, 0)},
-        ["32"] = {pos = Vector(0, -35, 30), ang = Angle(0, 0, 0)},
-        ["34"] = {pos = Vector(0, -40, 30), ang = Angle(0, 0, 0)},
-        ["36"] = {pos = Vector(0, -45, 30), ang = Angle(0, 0, 0)}
+        ["2"] = {pos = Vector(1, -2.3, 17.8), ang = Angle(0, 90, 0)},
+        ["4"] = {pos = Vector(7.3, 2.47, 17.8), ang = Angle(0, 90, 0)},
+        ["6"] = {pos = Vector(-5.4, 2.47, 17.8), ang = Angle(0, 90, 0)},
+        ["8"] = {pos = Vector(1, 7.25, 17.8), ang = Angle(0, 90, 0)},
+        ["10"] = {pos = Vector(7.3, 12.1, 17.8), ang = Angle(0, 90, 0)},
+        ["12"] = {pos = Vector(-5.4, 12.1, 17.8), ang = Angle(0, 90, 0)},
+        ["14"] = {pos = Vector(1, 17, 17.8), ang = Angle(0, 90, 0)},
+        ["16"] = {pos = Vector(7.3, 21.78, 17.8), ang = Angle(0, 90, 0)},
+        ["18"] = {pos = Vector(-5.4, 21.7, 17.8), ang = Angle(0, 90, 0)},
+        ["20"] = {pos = Vector(1, 26.6, 17.8), ang = Angle(0, 90, 0)},
+        ["22"] = {pos = Vector(7.3, 31.5, 17.8), ang = Angle(0, 90, 0)},
+        ["24"] = {pos = Vector(-5.4, 31.5, 17.8), ang = Angle(0, 90, 0)},
+        ["26"] = {pos = Vector(1, 36.3, 17.8), ang = Angle(0, 90, 0)},
+        ["28"] = {pos = Vector(7.3, 41.2, 17.8), ang = Angle(0, 90, 0)},
+        ["30"] = {pos = Vector(-5.4, 41.2, 17.8), ang = Angle(0, 90, 0)},
+        ["32"] = {pos = Vector(1, 45.95, 17.8), ang = Angle(0, 90, 0)},
+        ["34"] = {pos = Vector(7.3, 50.85, 17.8), ang = Angle(0, 90, 0)},
+        ["36"] = {pos = Vector(-5.4, 50.85, 17.8), ang = Angle(0, 90, 0)}
     }
 }
 
+-- Функция для разбивки суммы на фишки
+function BreakIntoChips(amount)
+    local chips = {}
+    local values = {1000, 500, 100, 50, 10, 5, 1} -- номиналы в порядке убывания
+    
+    for _, value in ipairs(values) do
+        while amount >= value do
+            table.insert(chips, value)
+            amount = amount - value
+        end
+    end
+    
+    return chips
+end
