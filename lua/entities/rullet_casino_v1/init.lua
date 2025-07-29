0d3177e04afbdd17b7de4f6d775264a24460e622
FURUSHKA <163753122+FYURi4@users.noreply.github.com>
2025-07-29 07:50:04 -0700

    Update init.lua

--- lua/entities/rullet_casino_v1/init.lua
+++ lua/entities/rullet_casino_v1/init.lua
@@ -1,55 +1,45 @@
--- sv_init.lua
-
 AddCSLuaFile("cl_init.lua")
 AddCSLuaFile("shared.lua")
 include("shared.lua")
 
-
-util.AddNetworkString("RoulettePlaceBet")
-util.AddNetworkString("RouletteShowHUD")
-util.AddNetworkString("RoulettePlayerSat")
-util.AddNetworkString("RoulettePlayerLeft")
-util.AddNetworkString("RouletteCameraUpdate")
-
--- Hooks for sitting/unsitting players
-hook.Add("PlayerEnteredVehicle", "Roulette_PlayerSat", function(ply, veh)
-    if not (IsValid(veh) and IsValid(ply)) then return end
-    if veh:GetNWBool("IsRouletteChair", false) then
-        local tableEnt = veh:GetNWEntity("RouletteTable")
-        if IsValid(tableEnt) and tableEnt.PlayerSat then
-            tableEnt:PlayerSat(ply, veh)
-        end
-    end
-end)
-
-hook.Add("PlayerLeaveVehicle", "Roulette_PlayerLeft", function(ply, veh)
-    if not (IsValid(veh) and IsValid(ply)) then return end
-    if veh:GetNWBool("IsRouletteChair", false) then
-        local tableEnt = veh:GetNWEntity("RouletteTable")
-        if IsValid(tableEnt) and tableEnt.PlayerLeft then
-            tableEnt:PlayerLeft(ply)
-        end
-    end
-end)
-
-
-local CAMERA_OFFSET = Vector(6, -8.5, 58) -- Смещение камеры относительно стола
-local CAMERA_ANGLE = Angle(90, 180, 0) -- Угол камеры (настроить по вкусу)
-local ROUND_WAITING = 0
-
--- Constants
-local CHAIR_OFFSETS = {
-    { pos = Vector(50, 13, 6.5), ang = Angle(0, 0, 0), addAng = Angle(0, 90, 0) },  
-    { pos = Vector(50, -24, 6.5), ang = Angle(0, 0, 0), addAng = Angle(0, 90, 0) },    
-    { pos = Vector(50, -61, 6.5), ang = Angle(0, 0, 0), addAng = Angle(0, 90, 0) },    
-    { pos = Vector(15, -100, 6.5), ang = Angle(0, 90, 0), addAng = Angle(0, 90, 0) }
-}
-
-local GMAN_OFFSET = Vector(-28, -35, -26)
-local GMAN_ANGLE_OFFSET = Angle(0, 0, 0)
+-- Константы вращения и шарика
 local WHEEL_OFFSET = Vector(5.96, 42.34, 26.75)
-local BALL_OFFSET_Z = -1.5
 local BALL_MODEL = "models/darkrpcasinoby3demc/play_sharik.mdl"
+
+local supportSpeed = 100
+local maxSpinSpeed = 500
+local rulletSpinSide = -1 -- -1 - right, 1 - Left (The side of the roulette wheel affects the side of the ball's rotation.)
+local accelerationAndDecelerationRullet = 6 -- seconds
+
+local sharikSpeed = 1200 
+
+local sharikR1 = 8.5
+local sharikR2 = 9
+local sharikR3 = 13.5
+
+local sharikBacklash = 0.05 -- the ball's backlash when it rolls along the 3rd radius
+local sharikH1 = -2.42
+local sharikH2 = -1.5
+local sharikStatus = "cell" -- ctll, r1, r2, r3, slowing down
+local ballBraking = 7
+
+local CELL_MAGNET_STRENGTH = 0.3  -- Сила "притяжения" к ячейке (0 = нет, 1 = мгновенное залипание)
+local CELL_SLOWDOWN_FACTOR = 0.95
+local roulette_order = {
+    0, 32, 15, 19, 4, 21, 2, 25, 17, 34,
+    6, 27, 13, 36, 11, 30, 8, 23, 10, 5,
+    24, 16, 33, 1, 20, 14, 31, 9, 22, 18,
+    29, 7, 28, 12, 35, 3, 26
+}
+local cellCount = #roulette_order
+local cellAngleStep = 360 / cellCount
+
+function ENT:GetCellByAngle(angle)
+    angle = angle % 360
+    local index = math.floor(angle / cellAngleStep) + 1
+    if index > cellCount then index = 1 end
+    return roulette_order[index]
+end
 
 function ENT:Initialize()
     self:SetModel("models/darkrpcasinoby3demc/table_rullet_casino.mdl")
@@ -59,298 +49,202 @@ function ENT:Initialize()
     self:SetUseType(SIMPLE_USE)
 
     local phys = self:GetPhysicsObject()
-    if IsValid(phys) then phys:EnableMotion(false) end
+    if IsValid(phys) then
+        phys:EnableMotion(false)
+    end
 
-    self.RoundState = ROUND_WAITING
-    self.BettingEndTime = 0
-    self.Players = {}
-
-    self:CreateChairs()
-    self:CreateGMan()
     self:CreateWheel()
     self:CreateBall()
-end
 
-function ENT:CreateChairs()
-    self.Chairs = {}
-    for i, offset in ipairs(CHAIR_OFFSETS) do
-        local chairPos = self:GetPos() + self:GetForward() * offset.pos.x + self:GetRight() * offset.pos.y + self:GetUp() * offset.pos.z
-        local seat = ents.Create("prop_vehicle_prisoner_pod")
-        seat:SetModel("models/nova/airboat_seat.mdl")
-        seat:SetKeyValue("vehiclescript", "")
-        seat:SetPos(chairPos)
-        seat:SetAngles(self:GetAngles() + offset.ang + offset.addAng)
-        seat:Spawn()
-        seat:SetParent(self)
+    self.RouletteSpinSpeed = rulletSpeed
+    self.RouletteSpinning = false
+    self.RouletteAngle = 0
+    self.RouletteSpinPhase = "idle"
+    self.RouletteSpinStartTime = 0
+    self.RouletteCurrentSpeed = 0
+    self.BallSpeed = 0
+    self.BallHoldSpeed = 0
+    self.BallRadius = sharikR1
+    self.BallHeight = sharikH1
+    self.BallAttachedToWheel = true
+    self.BallLockedInCell = false
+    self.BallLockAngleOffset = 0
 
-        seat:SetNoDraw(true)
-        seat:SetColor(Color(0, 0, 0, 0))
-        seat:SetRenderMode(RENDERMODE_TRANSALPHA)
-        seat:SetCollisionGroup(COLLISION_GROUP_WEAPON)
-        seat:SetNWEntity("RouletteTable", self)
-        seat:SetNWBool("IsRouletteChair", true)
-
-        local chair = ents.Create("prop_dynamic")
-        chair:SetModel("models/darkrpcasinoby3demc/chair_co.mdl")
-        chair:SetPos(chairPos)
-        chair:SetAngles(self:GetAngles() + offset.ang)
-        chair:SetParent(seat)
-        chair:Spawn()
-        chair:DrawShadow(false)
-
-        self.Chairs[i] = seat
-        self:DeleteOnRemove(seat)
-        self:DeleteOnRemove(chair)
-    end
-end
-
-function ENT:CreateGMan()
-    -- GMan
-    self.GMan = ents.Create("npc_gman")
-    if IsValid(self.GMan) then
-        local localOffset = GMAN_OFFSET
-        local worldPos = self:GetPos()
-            + self:GetForward() * localOffset.x
-            + self:GetRight() * localOffset.y
-            + self:GetUp() * localOffset.z
-
-        self.GMan:SetModel("models/gman.mdl")
-        self.GMan:SetPos(worldPos)
-        self.GMan:SetAngles(self:GetAngles())
-        self.GMan:Spawn()
-        self.GMan:Activate()
-
-        -- Установка состояния NPC
-        self.GMan:SetNPCState(NPC_STATE_SCRIPT)
-        self.GMan:SetSolid(SOLID_NONE)
-        self.GMan:SetMoveType(MOVETYPE_NONE) -- NPC теперь "заморожен"
-        self.GMan:SetHealth(99999)
-        self.GMan:SetMaxHealth(99999)
-        self.GMan:SetSchedule(SCHED_IDLE_STAND)
-        self.GMan:AddRelationship("player D_LI 99")
-        self.GMan:CapabilitiesAdd(CAP_ANIMATEDFACE + CAP_TURN_HEAD)
-
-        -- Принудительно задать анимацию
-        self.GMan:ResetSequence(self.GMan:LookupSequence("lineidle01"))
-
-        -- Родитель — стол
-        self.GMan:SetParent(self)
-
-    
-        self:DeleteOnRemove(self.GMan)
-    end
-end
+    -- Начальный угол шарика (будет обновляться при остановке)
+    self.BallAngle = 180
+    self.LastBallAngle = 180 -- Сохраняем последний угол шарика
+end 
 
 function ENT:CreateWheel()
     self.Wheel = ents.Create("prop_dynamic")
     if not IsValid(self.Wheel) then return end
 
-    local wheelPos = self:GetPos() + self:GetForward() * WHEEL_OFFSET.x + self:GetRight() * WHEEL_OFFSET.y + self:GetUp() * WHEEL_OFFSET.z
+    local pos = self:GetPos() + self:GetForward() * WHEEL_OFFSET.x +
+                self:GetRight() * WHEEL_OFFSET.y +
+                self:GetUp() * WHEEL_OFFSET.z
+
     self.Wheel:SetModel("models/darkrpcasinoby3demc/table_rullet_casino_detail.mdl")
-    self.Wheel:SetPos(wheelPos)
+    self.Wheel:SetPos(pos)
     self.Wheel:SetAngles(self:GetAngles())
     self.Wheel:SetParent(self)
     self.Wheel:Spawn()
-
-    local wheelPhys = self.Wheel:GetPhysicsObject()
-    if IsValid(wheelPhys) then wheelPhys:EnableMotion(false) end
 
     self:DeleteOnRemove(self.Wheel)
 end
 
 function ENT:CreateBall()
     self.Ball = ents.Create("prop_dynamic")
-    if not IsValid(self.Ball) or not IsValid(self.Wheel) then return end
+    if not IsValid(self.Ball) then return end
 
     self.Ball:SetModel(BALL_MODEL)
-    self.Ball:SetPos(self.Wheel:GetPos() + self.Wheel:GetUp() * BALL_OFFSET_Z)
-    self.Ball:SetAngles(self:GetAngles())
-    self.Ball:SetParent(self.Wheel)
+    self.Ball:SetAngles(Angle(0, 0, 0))
+    self.Ball:SetMoveType(MOVETYPE_NONE)
+    self.Ball:SetParent(nil)
     self.Ball:Spawn()
-
-    local ballPhys = self.Ball:GetPhysicsObject()
-    if IsValid(ballPhys) then ballPhys:EnableMotion(false) end
-
     self:DeleteOnRemove(self.Ball)
 end
 
-function ENT:PlayerSat(ply, chair)
-    if not (IsValid(ply) and ply:IsPlayer()) then return end
-    
-    self.Players[ply] = {
-        chair = chair,
-        cameraPos = self:GetPos() + self:GetForward() * CAMERA_OFFSET.x + 
-                   self:GetRight() * CAMERA_OFFSET.y + 
-                   self:GetUp() * CAMERA_OFFSET.z,
-        cameraAng = self:GetAngles() + CAMERA_ANGLE
-    }
-    
-    ply:SetNWEntity("RouletteChair", chair)
-    ply:SetNWEntity("RouletteTable", self)
-    
-    -- Отправляем данные о камере клиенту
-    net.Start("RouletteCameraUpdate")
-        net.WriteBool(true) -- Включить камеру
-        net.WriteVector(self.Players[ply].cameraPos)
-        net.WriteAngle(self.Players[ply].cameraAng)
-    net.Send(ply)
-    
-    net.Start("RouletteShowHUD") 
-        net.WriteBool(true) 
-    net.Send(ply)
-    
-    net.Start("RoulettePlayerSat") 
-        net.WriteEntity(self) 
-    net.Send(ply)
-end
+function ENT:Think()
+    local now = CurTime()
+    local elapsed = now - self.RouletteSpinStartTime
+    local accelTime = accelerationAndDecelerationRullet
 
+    -- === ФАЗЫ ВРАЩЕНИЯ РУЛЕТКИ ===
+    if self.RouletteSpinPhase == "accel" then
+        local t = math.min(elapsed / accelTime, 1)
+        self.RouletteCurrentSpeed = Lerp(t, supportSpeed, maxSpinSpeed)
 
-function ENT:PlayerLeft(ply)
-    if not (IsValid(ply) and ply:IsPlayer()) then return end
-    
-    if self.Players[ply] then
-        -- Отправляем команду отключить кастомную камеру
-        net.Start("RouletteCameraUpdate")
-            net.WriteBool(false) -- Выключить камеру
-        net.Send(ply)
-        
-        self.Players[ply] = nil
-    end
-    
-    ply:SetNWEntity("RouletteChair", NULL)
-    ply:SetNWEntity("RouletteTable", NULL)
-    
-    net.Start("RouletteShowHUD") 
-        net.WriteBool(false) 
-    net.Send(ply)
-    
-    net.Start("RoulettePlayerLeft") 
-        net.WriteEntity(self) 
-    net.Send(ply)
-end
+        self.BallAttachedToWheel = true
+        self.BallLockedInCell = false
 
-net.Receive("RoulettePlaceBet", function(len, ply)
-    local amount = net.ReadUInt(32)
-    local bet = net.ReadString()
-    
-    -- Проверки валидности
-    if not IsValid(ply) then return end
-    
-    local tableEnt = ply:GetNWEntity("RouletteTable")
-    if not IsValid(tableEnt) then
-        DarkRP.notify(ply, 1, 4, "Вы должны сидеть за столом!")
-        return
-    end
-
-    if not ply:canAfford(amount) then
-        DarkRP.notify(ply, 1, 4, "Недостаточно денег!")
-        return
-    end
-    
-    if amount < 1 or amount > 1000 then
-        DarkRP.notify(ply, 1, 4, "Ставка должна быть от 1 до 1000")
-        return
-    end
-    
-    local betData
-    for group, bets in pairs(POSITION) do
-        if bets[bet] then
-            betData = bets[bet]
-            break
+        if t >= 1 then
+            self.RouletteSpinPhase = "support"
+            self.RouletteSpinStartTime = now
+            self.BallAttachedToWheel = false
+            self.BallRadius = sharikR3
+            self.BallHeight = sharikH2
+            self.BallSpeed = self.RouletteCurrentSpeed
+            self.BallHoldSpeed = self.BallSpeed
         end
-    end
-    
-    if not betData then
-        DarkRP.notify(ply, 1, 4, "Неверный тип ставки!")
-        return
-    end
-    
-    -- Разбиваем сумму на фишки
-    local chipsToSpawn = BreakIntoChips(amount)
-    local firstChip = nil
-    local basePos = tableEnt:LocalToWorld(betData.pos)
-    local baseAng = tableEnt:LocalToWorldAngles(betData.ang or Angle(0,0,0))
-    
-    -- Создаем фишки с эффектом появления
-    for i, chipValue in ipairs(chipsToSpawn) do
-        timer.Simple((i-1)*0.05, function() -- Задержка для эффекта последовательного появления
-            if not IsValid(tableEnt) then return end
-            
-            local chip = ents.Create("prop_dynamic")
-            chip:SetModel(CHIP_MODEL)
-            
-            -- Позиция с учетом высоты стопки и случайного смещения
-            local pos = basePos + Vector(
-                math.Rand(-CHIP_OFFSET, CHIP_OFFSET),
-                math.Rand(-CHIP_OFFSET, CHIP_OFFSET),
-                (i-1) * CHIP_HEIGHT
-            )
-            
-            local ang = baseAng
-            ang:RotateAroundAxis(ang:Up(), math.Rand(-15, 15))
-            
-            chip:SetPos(pos)
-            chip:SetAngles(ang)
-            
-            -- Установка текстуры фишки
-            for texId, value in pairs(CHIP_VALUES) do
-                if chipValue == value then
-                    chip:SetSkin(texId)
-                    break
-                end
+
+    elseif self.RouletteSpinPhase == "support" then
+        local t = math.min(elapsed / accelTime, 1)
+        self.RouletteCurrentSpeed = Lerp(t, maxSpinSpeed, supportSpeed)
+        self.BallSpeed = self.BallHoldSpeed
+
+        if t >= 1 then
+            self.RouletteSpinPhase = "ball_slowing"
+            self.RouletteSpinStartTime = now
+            self.BallSlowStartSpeed = self.BallSpeed
+        end
+
+        elseif self.RouletteSpinPhase == "ball_slowing" then
+            self.RouletteCurrentSpeed = supportSpeed
+
+            local t = math.min((now - self.RouletteSpinStartTime) / ballBraking, 1)
+            self.BallSpeed = Lerp(t, self.BallSlowStartSpeed, 0)
+
+            -- Плавное снижение шарика по радиусам
+            local radiusProgress = math.min(t / 0.66, 1) -- 66% времени на снижение
+            if radiusProgress < 0.5 then
+                -- Переход от R3 к R2
+                local subProgress = radiusProgress * 2
+                self.BallRadius = Lerp(subProgress, sharikR3, sharikR2)
+                self.BallHeight = Lerp(subProgress, sharikH2, sharikH1)
+            else
+                -- Переход от R2 к R1
+                local subProgress = (radiusProgress - 0.5) * 2
+                self.BallRadius = Lerp(subProgress, sharikR2, sharikR1)
+                self.BallHeight = sharikH1
             end
-            
-            chip:Spawn()
-            
-            -- Фиксация фишки
-            local phys = chip:GetPhysicsObject()
-            if IsValid(phys) then
-                phys:EnableMotion(false)
+
+            if self.BallSpeed <= 1 then
+                self.BallSpeed = 0
+                self.RouletteSpinPhase = "decel"
+                self.RouletteSpinStartTime = now
+                self.BallLockedInCell = true
+                self.BallLockAngleOffset = self.BallAngle - self.RouletteAngle
+                
+                -- Сохраняем последний угол шарика
+                self.LastBallAngle = self.BallAngle
             end
-            
-            -- Эффект появления (увеличение)
-            chip:SetModelScale(0.1, 0)
-            chip:SetModelScale(1, 0.2)
-            
-            -- Привязка к столу
-            chip:SetParent(tableEnt)
-            
-            -- Сохранение первой фишки
-            if i == 1 then
-                firstChip = chip
-            end
-            
-            -- Автоудаление
-            timer.Simple(30, function()
-                if IsValid(chip) then chip:Remove() end
-            end)
-        end)
+
+    elseif self.RouletteSpinPhase == "decel" then
+        local t = math.min(elapsed / accelerationAndDecelerationRullet, 1)
+        self.RouletteCurrentSpeed = Lerp(t, supportSpeed, 0)
+
+        if self.BallSpeed <= 1 and not self.ResultLocked then
+            self.BallSpeed = 0
+            self.BallLockedInCell = true
+            self.BallLockAngleOffset = self.BallAngle - self.RouletteAngle
+            local ballCellNumber = self:GetCellByAngle(self.BallAngle)
+            self.CurrentResult = ballCellNumber
+            self.ResultLocked = true
+            print("Ball stopped in cell: ", ballCellNumber)
+        end
+
+        if t >= 1 then
+            self.RouletteSpinPhase = "idle"
+        end
+
+    elseif self.RouletteSpinPhase == "idle" then
+        self.RouletteCurrentSpeed = 0
     end
-    
-    -- Списываем деньги
-    ply:addMoney(-amount)
-    
-    -- Сохраняем ставку
-    if IsValid(firstChip) then
-        tableEnt.PlayerBets = tableEnt.PlayerBets or {}
-        tableEnt.PlayerBets[ply] = tableEnt.PlayerBets[ply] or {}
-        table.insert(tableEnt.PlayerBets[ply], {
-            chip = firstChip,
-            amount = amount,
-            bet = bet,
-            allChips = chipsToSpawn
-        })
+
+    -- Вращение барабана
+    local angleDelta = self.RouletteCurrentSpeed * FrameTime() * rulletSpinSide
+    self.RouletteAngle = (self.RouletteAngle + angleDelta) % 360
+    local ang = self:GetAngles()
+    ang:RotateAroundAxis(self:GetUp(), self.RouletteAngle)
+    self.Wheel:SetAngles(Angle(ang.p, ang.y, ang.r))
+
+    -- Вращение шарика
+    if IsValid(self.Ball) then
+        if self.BallAttachedToWheel then
+            self.BallAngle = self.RouletteAngle
+        elseif self.BallLockedInCell then
+            self.BallAngle = self.RouletteAngle + (self.BallLockAngleOffset or 0)
+        else
+            self.BallAngle = (self.BallAngle + self.BallSpeed * FrameTime() * rulletSpinSide) % 360
+        end
+
+        local basePos = self:GetPos() +
+            self:GetForward() * WHEEL_OFFSET.x +
+            self:GetRight() * WHEEL_OFFSET.y +
+            self:GetUp() * WHEEL_OFFSET.z
+
+        local correctedBallAngle = self.BallAngle + 180
+        local localBallPos = Vector(
+            math.cos(math.rad(correctedBallAngle)) * self.BallRadius,
+            math.sin(math.rad(correctedBallAngle)) * self.BallRadius,
+            self.BallHeight
+        )
+
+        local rotatedBallPos = LocalToWorld(localBallPos, Angle(0, 0, 0), basePos, self:GetAngles())
+        self.Ball:SetPos(rotatedBallPos)
     end
-    
-    DarkRP.notify(ply, 0, 4, "Вы поставили "..DarkRP.formatMoney(amount).." на "..bet)
-end)
+
+    self:NextThink(now)
+    return true
+end 
+
+
+function ENT:Use(activator, caller)
+    if self.RouletteSpinPhase == "idle" then
+        self.RouletteSpinPhase = "accel"
+        self.RouletteSpinStartTime = CurTime()
+        self.BallRadius = sharikR1
+        self.BallHeight = sharikH1
+        self.BallSpeed = 0
+        self.BallAttachedToWheel = true
+        
+        -- Устанавливаем начальный угол шарика из сохраненного значения
+        self.BallAngle = self.LastBallAngle
+        self.ResultLocked = false
+    end
+end
+
 
 function ENT:OnRemove()
-    for ply, _ in pairs(self.Players) do
-        if IsValid(ply) and IsValid(ply.RouletteCamera) then
-            ply:SetViewEntity(NULL)
-            ply.RouletteCamera:Remove()
-            ply.RouletteCamera = nil
-        end
-    end
+
 end
